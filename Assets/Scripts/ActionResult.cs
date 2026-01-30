a306fc989776bd3b8bc20bf44e4183dff55575e9
Livour <86783121+Livour@users.noreply.github.com>
2026-01-30 12:13:37 +0200

    feat(core): Add ActionResult enum for game state management

---
+++ Assets/Scripts/ActionResult.cs
@@ -0,0 +1,6 @@
+public enum ActionResult
+{
+    Success,            // Valid mask placed on new house
+    IllegalMove,        // Clicked on grayed house
+    AlreadyVisited,     // repeated mask on same house
+}
